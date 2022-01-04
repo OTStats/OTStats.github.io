23a0699067b8070365dc17b40ec6f0822753c421
Owen Thompson <octhompson19@gmail.com>
2022-01-03 20:02:58 -0600

    update udisc-rounds and aaron-rodgers TD's

--- _R/WIP/udisc-rounds.R
+++ _R/WIP/udisc-rounds.R
@@ -1,8 +1,13 @@
 
+
 # -- Load libraries
 library(tidyverse)
+install.packages("ggheatmap")
+source("https://raw.githubusercontent.com/iascchen/VisHealth/master/R/calendarHeat.R")
+# Time Series Calendar Maps:
+# http://www.columbia.edu/~sg3637/blog/Time_Series_Heatmaps.html
 
-df <- read_csv("Downloads/UDisc Scorecards.csv") %>% 
+df <- read_csv("~/Downloads/UDisc Scorecards (1).csv") %>% 
   janitor::clean_names()
 
 long_df <- df %>% 
@@ -18,9 +23,31 @@ par_df <- long_df %>%
   rename(par = score) %>% 
   select(-player_name)
 
-long_df %>% 
+long_score_df <- long_df %>% 
   filter(player_name != "Par") %>% 
   left_join(par_df, 
             by = c("course_name", "layout_name", "date", "hole"))
 
-            
+date_count <- df %>% 
+  filter(str_detect(player_name, "OT|Owen")) %>% 
+  group_by(date = lubridate::as_date(date)) %>% 
+  count()
+
+summary_score_df <- long_score_df %>% 
+  filter(str_detect(player_name, "OT|Owen")) %>% 
+  filter(score != 0) %>% 
+  mutate(diff = score - par) %>% 
+  group_by(date = lubridate::as_date(date)) %>% 
+  summarize(sum_diff = sum(diff))
+
+
+calendarHeat(dates = date_count$date, 
+             values = date_count$n, 
+             color = "r2b", 
+             ncolors = 2, 
+             varname = "Disc Golf Rounds")
+calendarHeat(dates = summary_score_df$date, 
+             values = summary_score_df$sum_diff, 
+             color = "r2b", 
+             ncolors = 5, 
+             varname = "Scores")
--- _R/nfl-pbp-aaron-rodgers.R
+++ _R/nfl-pbp-aaron-rodgers.R
@@ -1,21 +1,36 @@
-library(tidyverse)
+
+# -- Load libraries
 install.packages(c("nflfastR", "furrr"))
+library(tidyverse)
 library(nflfastR)
 library(furrr)  # required for parallel processing for `nflfastR::fast_scraper`
 
-gb_results <- fast_scraper_schedules(2005:2020) %>% 
+# future::plan("multisession")  # Use this for parallel processing
+
+# -- Get Green Bay games from 2005 to present
+gb_results <- fast_scraper_schedules(2005:2021) %>% 
   filter(away_team == "GB" | home_team == "GB")
 
 gb_pbp <- gb_results %>% 
   pull(game_id) %>% 
-  fast_scraper(pp = TRUE)
+  fast_scraper()
+
+beepr::beep()
 
 aaron_td <- gb_pbp %>% 
   filter(passer_player_name == "A.Rodgers", 
          td_team == "GB", 
-         pass_touchdown == 1) %>% 
+         pass_touchdown == 1) #%>% 
   filter(season_type == "REG")
 
+gb_results %>% 
+  transmute(game_id, 
+            season, 
+            gameday, 
+            weekday, 
+            opponent = if_else(home_team == "GB", away_team, home_team))
+
+aaron_td
 
 aaron_td %>% 
   ggplot(aes(x = air_yards, y = yards_after_catch)) + 
@@ -29,9 +44,9 @@ aaron_td %>%
 aaron_td %>% 
   count(receiver_player_name, sort = T)
 
-rodgers %>% 
-  group_by(receiver) %>% 
-  summarize(n_seasons = n_distinct(season), 
-            seasons = str_c(min(season), "-", max(season)), 
-            touchdowns = n()) %>% 
-  arrange(desc(touchdowns))
+# aaron_td %>% 
+#   group_by( = receiver_player_name) %>% 
+#   summarize(n_seasons = n_distinct(season), 
+#             seasons = str_c(min(season), "-", max(season)), 
+#             touchdowns = n()) %>% 
+#   arrange(desc(touchdowns))
